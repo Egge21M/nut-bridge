5e44b52a1bbdce40ddc66ee8587f74d49dd9f70b
Egge <egge@getcurrent.io>
2025-03-17 13:53:01 +0100

    added nutWalletManager and nutzap logic

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,21 +1,30 @@
-import { getEncodedToken, Proof } from "@cashu/cashu-ts";
+import {
+  CashuMint,
+  CashuWallet,
+  getEncodedToken,
+  Proof,
+} from "@cashu/cashu-ts";
 import { MINT_URL } from "./config";
 import { resolve } from "path";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { MintCommunicator } from "almnd";
+import { nutWalletManager, publishNutZap } from "./nostr";
 
 const mintComm = new MintCommunicator(MINT_URL, {
   initialPollingTimeout: { mint: 10000, melt: 10000, proof: 10000 },
   backoffFunction: (r) => 5000 * Math.pow(2, r),
 });
 
+const wallet = new CashuWallet(new CashuMint(MINT_URL));
+
 export async function createInvoiceAndHandlePayment(amount: number) {
   const { quote, request } = await mintComm.getMintQuote(amount);
   const sub = mintComm.pollForMintQuote(quote);
-  sub.on("paid", async (r) => {
-    const proofs = await mintComm.getProofs(amount, r);
-    const token = turnProofsIntoToken(proofs);
-    console.log(token);
+  sub.on("paid", async () => {
+    const proofs = await wallet.mintProofs(amount, quote, {
+      p2pk: { pubkey: nutWalletManager.pubkey! },
+    });
+    await publishNutZap(proofs);
     sub.cancel();
   });
   return request;
