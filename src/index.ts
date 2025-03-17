ca9ed0ed006658e6aaa12efe335b38949195a53a
Egge <egge@getcurrent.io>
2025-03-17 13:53:07 +0100

    added start up function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { USERNAME } from "./config";
 import { lud16Controller } from "./controller";
+import { nutWalletManager } from "./nostr";
 
 const app = express();
 
 app.get("/.well-known/lnurlp/" + USERNAME, lud16Controller);
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
-});
+async function startServer() {
+  await nutWalletManager.init();
+
+  app.listen(8080, () => {
+    console.log("Server running on port 8080");
+  });
+}
+
+startServer();
