2a4f96bfc46fe2ca7f9d03dea387576aea6c33e7
Egge <egge@getcurrent.io>
2025-03-18 17:04:24 +0100

    added more config env vars

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 import { generateSecretKey, getPublicKey } from "nostr-tools";
 
-export const USERNAME = "egge";
+export const USERNAME = process.env.USERNAME;
 export const PORT = process.env.PORT || 8080;
-export const HOSTNAME = "https://nutzap.cash";
+export const HOSTNAME = process.env.HOSTNAME;
 export const MINT_URL = "https://mint.minibits.cash/Bitcoin";
 
 export const NOSTR_HEX_PUBKEY =
   "0a2f6049f3f237f71baf0ec90b2cd2b5ce52621adaba03fa9b84b212ed56022d";
 
-export const MIN_AMOUNT = 1000;
-export const MAX_AMOUNT = 10000;
+export const MIN_AMOUNT = process.env.MIN_AMOUNT;
+export const MAX_AMOUNT = process.env.MAX_AMOUNT;
 
 export const DEFAULT_RELAYS = [
   "wss://relay.primal.net",
